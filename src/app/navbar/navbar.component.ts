3628914d62be014bec6aa6041059590003f592b3
Thenew2 <hernandezeddier@gmail.com>
2025-02-09 23:55:30 -0500

    Add sidebar and navbar components with corresponding styles and templates

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  sidebarVisible = false;
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+    // Emitir un evento o usar un servicio si necesitas comunicar el estado al componente padre
+  }
+}
